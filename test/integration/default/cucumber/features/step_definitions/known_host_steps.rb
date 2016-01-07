1929a5d287dc5e1c2a408e9405cef68239d929a1
Barney Hanlon <barney@shrikeh.net>
2016-01-07 08:06:30 +0000

    Add step definitions folder

---
+++ test/integration/default/cucumber/features/step_definitions/known_host_steps.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+Given(/^the deploy user is "([^"]*)"$/) do |arg1|
+  # Placeholder step
+end
+
+Given(/^git is installed on the server/) do
+
+end
+
+When(/^I checkout my private repository$/) do
+  @git_clone = `sudo -u deploy git clone git@github.com:inviqa/chef-deploy-user.git /etc/deploy/chef-deploy-user`
+end
+
+Then(/^I am not asked to verify the host$/) do
+  ::STDOUT.puts @git_clone
+end
