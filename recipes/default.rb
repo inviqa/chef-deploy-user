982d7a15c4b5449bb5bf038010ead1b4db062915
Marco Massari Calderone <mmassari@inviqa.com>
2016-11-29 13:01:29 -0600

    fix know_hosts update process

--- recipes/default.rb
+++ recipes/default.rb
@@ -69,7 +69,6 @@ directory ssh_dir_path do
   group node['deploy_user']['gid']
   mode '0700'
   action :create
-  force_unlink true
 end
 
 deploy_keys = node['deploy_user']['private_keys'] || []
@@ -105,9 +104,7 @@ end
 Chef::Log.debug 'Add the known_host entries.'
 node['deploy_user']['ssh_known_hosts_entries'].each do |known_host_entry|
   ssh_known_hosts_entry known_host_entry['host'] do
-    file ssh_known_hosts_path
-    key_type known_host_entry['key_type']
-    key known_host_entry['key']
+    key known_host_entry['key'] if known_host_entry['key']
   end
 end
 
